6ad53d0f2849df97e42a0de4dcdfb9424bf82fde
Miller, Andrew | Andrew | ADPDE <andrew.miller@rakuten.com>
2023-08-29 11:48:46 +0900

    Namespace tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,12 +1,15 @@
 <?php
 
-use GuzzleHttp\Psr7\Stream;
+namespace Linkshare\Api\RanReporting\Tests;
 
-abstract class TestCase extends PHPUnit_Framework_TestCase
+use GuzzleHttp\Psr7\Stream;
+use PHPUnit\Framework\TestCase as BaseTestCase;
+
+abstract class TestCase extends BaseTestCase
 {
     public const PATH_TO_SAMPLE_REPORT = __DIR__ . '/data/signature-orders.csv';
 
-    protected function createSampleReportStream()
+    protected function createSampleReportStream(): Stream
     {
         return new Stream(fopen(self::PATH_TO_SAMPLE_REPORT, 'r'));
     }
